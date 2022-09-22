ff47c74b3a98a8e80cc7b1a47024f5a076188f58
nerdommicro <mn218@nerdymail.com>
2022-09-22 10:13:06 -0400

    mn

--- zendapp/public/index.php
+++ zendapp/public/index.php
@@ -1,12 +1,14 @@
 //add the autoload in here
 <? php
+use ZendApp\Model;
 
-autoLoader = new AutoLoader(__DIR__); //put in index.php
-?>
-<?php
 spl_autoload_register(function ($class) {
   require_once __DIR__
     . DIRECTORY_SEPARATOR
     . str_replace('\\', DIRECTORY_SEPARATOR, $class)
     . '.php';
 });
+
+autoLoader = new AutoLoader(__DIR__); //put in index.php
+
+
--- zendapp/src/ZendApp/Model/Product.php
+++ zendapp/src/ZendApp/Model/Product.php
@@ -6,40 +6,38 @@ namespace ZendApp\Model;
 //* Extensible superclass definition
 class Product
 {
-  protected string $sku;
-  protected string $title;
-  protected string $imageUrl;
-  protected string $tags;
-  protected string $description;
-  protected int $quantity;
-  protected int $id = null;
+  public const error1 = "This is an error";
 
+  //* add data types in the constructor --
+  //! where to add the default value when inheriting?? at the end?
 
-  // ! should add in the data types in the constructor --
-  public function __construct(string $sku, string $title, string $imageUrl, 
-  string $tags, string $description, int $quantity, int $id=null)
-  {
-    $this->sku = $sku;
-    $this->title = $title;
-    $this->imageUrl = $imageUrl;
-    $this->tags = $tags;
-    $this->description = $description;
-    $this->quantity = $quantity;
-    $this->id = $id;
+  public function __construct(
+    protected string $sku,
+    protected string $title,
+    protected string $imageUrl,
+    protected string $tags,
+    protected string $description,
+    protected int $quantity,
+    protected int $id = null
+  ) {
   }
 
-  public function __get($value)
+  public function __get($value) //!don't have to add this to the subclass
   {
-    return $this->$value ?? null; //! -- add the $ dollar sign to the right of arrow -> value
+    return $this->$value ?? null; //* $ dollar sign to the right of arrow 
   }
 
   public function __toString()
   {
-    return $this->title; //!but how to inherit this?
+    return $this->title; //!this will be inherited
+  }
+
+  public function getInfo() :string  
+  {
+    return "SKU:" . $this->sku . ' Title: ' . $this->title;
   }
 }
 
 
 $product1 = new Product('sku1', 'title1', 'image.jpg', 'tag1 tag2', 'description1 description', 1000, 1);
-
 echo $product1; //! will output tostring version
